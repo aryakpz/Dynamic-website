1644fe3991ebdddaa7cb56c22a4f3e46cad2d5e4
Code Malayalam <vrindhatg6@gmail.com>
2024-08-13 16:38:01 +0530

    " almost "

--- keyboard/key.js
+++ keyboard/key.js
@@ -1,55 +1,116 @@
-    const letters = document.querySelectorAll('.letter'); 
-    const textarea =document.getElementById('content'); 
-    const clickel = document.querySelector('.click'); 
-    const keyel=document.querySelector('.keys')
+const letters = document.querySelectorAll('.letter');
+const textarea = document.getElementById('content');
 
-    const spaceel=document.querySelector('#space')
-    const back=document.querySelector("#back")
-    
-    let index=0
-    var ar=[]
+const clickel = document.querySelector('.click');
+const keyel = document.querySelector('.keys')
 
-    clickel.addEventListener('dblclick',()=>{
-        textarea.value+=letters[index].textContent   
-        
-    })
+const spaceel = document.querySelector('#space')
+const backel = document.querySelector("#back")
 
-    clickel.addEventListener('click',()=>{
-        letters[index].style.backgroundColor='white'
-        
-         index=(index + 1) 
-         if(index >=letters.length )
+
+
+const infoel=document.querySelector('.info')
+
+const subel=document.querySelector('.sub')
+
+
+let index = 0
+var ar = []
+let movetime, displaytime;
+let f = 0
+function display() {
+
+
+    // const crnt = letters[index].textContent
+    // console.log(crnt)
+    if (letters[index].textContent === "Backspace ") {
+
+        textarea.value = textarea.value.slice(0, -1)
+    }
+
+    else if (letters[index].textContent === "space" ) {
+
+        textarea.value += '  '
+
+    }
+    else if(letters[index].textContent === "Tab")
+        {
+            textarea.value += '     '  
+        }
+        else if(letters[index].textContent === "Enter")
             {
-                index=0  
-            }     
-         letters[index].style.backgroundColor='skyblue'
+                textarea.value += '\n'  
+            }
 
-    })               
-                                             
+    else if (letters[index].textContent === "CapsLk") {
+        if (f == 0)
+        {
+            f = 1
+        }
+        else
+        {
+            f = 0
+        }
+    }
+    else if(letters[index].textContent === "Fn" || letters[index].textContent === "Cntrl" || letters[index].textContent === "alt" || letters[index].textContent === "Shift" || letters[index].textContent === "AltGr")
+        {
+            textarea.value += ''
+        }
+    else {
 
-// keyel.addEventListener('click',(e)=>{
-//     const letter=e.target.textContent
+        if (f == 1) {
+            textarea.value += letters[index].textContent.toUpperCase()
+        }
+        else {
+            textarea.value += letters[index].textContent.toLowerCase()
+        }
+    }
 
-//     if(letter==='Backspace')
-//         {
-//             backr()       
-//         }
-//         else{
-//     textarea.value+=letter
-//         }
-   
-// })
+}
 
-// spaceel.addEventListener('click',()=>{                                                                                                    
-//     const sub=document.createElement('span')
-//     sub.innerHTML='&nbsp;'
-//     textarea.value+=sub.textContent
+function move() {
 
+    letters[index].style.backgroundColor = 'white'
 
-// })
+    index = (index + 1)
+    if (index >= letters.length) {
+        index = 0
+    }
+    letters[index].style.backgroundColor = 'skyblue'
+
+    console.log(letters[index].textContent)
+}
 
 
-// function backr(){
-// console.log("fnjnjf")
-// }
-   
+
+clickel.addEventListener('mouseover', () => {
+    movetime = setInterval(move, 1000)
+
+})
+
+
+clickel.addEventListener('click', () => {
+
+    display()
+
+},1000)
+
+
+
+clickel.addEventListener('mouseleave', () => {
+
+// display()
+clearInterval(movetime)
+
+
+})
+
+
+
+
+infoel.addEventListener('click',()=>{
+    subel.style.display=subel.style.display==='block'?'none':'block'
+
+
+ })
+               
